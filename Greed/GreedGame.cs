bf55a07cfd743d348689671d1fecdbec933b48b8
Jordan <hammerleejordan@gmail.com>
2021-03-18 07:21:23 -0700

    Finish First Example

--- Greed/GreedGame.cs
+++ Greed/GreedGame.cs
@@ -1,7 +1,97 @@
-﻿namespace GreedKata.Greed
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GreedKata.Greed
 {
     public class GreedGame
     {
-        
+        private readonly Dictionary<int, int> singleDiceScoreLookup = new Dictionary<int, int>()
+        {
+            {1, 100},
+            {2, 0},
+            {3, 0},
+            {4, 0},
+            {5, 50},
+            {6, 0},
+        };
+
+        private readonly Dictionary<int, int> tripleDiceScoreLookup = new Dictionary<int, int>()
+        {
+            {1, 1000},
+            {2, 200},
+            {3, 300},
+            {4, 400},
+            {5, 500},
+            {6, 600},
+        };
+
+        public int Calculate(int[] diceRolls)
+        {
+            int calculatedScore = 0;
+
+           var rollCounts = GetRollCounts(diceRolls);
+
+           if (rollCounts.Any(x => x.Value > 2))
+           {
+               var tripleRollKeys = rollCounts
+                   .Where(x => x.Value > 2)
+                   .Select(x => x.Key);
+
+               foreach (int key in tripleRollKeys)
+               {
+                   calculatedScore += tripleDiceScoreLookup[key];
+                   rollCounts[key] -= 3;
+                   if (rollCounts[key] == 0)
+                       rollCounts.Remove(key);
+               }
+           }
+
+
+            calculatedScore += rollCounts
+                .Sum(die => singleDiceScoreLookup[die.Key]);
+
+            return calculatedScore;
+        }
+
+        private bool HasTriple(int[] dice, out int diceNumber)
+        {
+            bool foundUnique = false;
+            diceNumber = 0;
+
+            for (int i = 0; i < dice.Length; i++)
+            {
+                int rollNumber = dice[i];
+                var rollCount = 1;
+
+                for (int j = i + 1; j < dice.Length; j++)
+                {
+                    if (rollNumber == dice[j])
+                        rollCount++;
+                    else if (!foundUnique)
+                    {
+                        foundUnique = true;
+                        i = j;
+                    }
+                }
+
+                if (rollCount > 2)
+                {
+                    diceNumber = rollNumber;
+                    return true;
+                }
+
+                foundUnique = false;
+            }
+
+            return false;
+        }
+
+        private Dictionary<int, int> GetRollCounts(int[] rolls)
+        {
+            var uniqueRolls = rolls.ToHashSet();
+
+            return uniqueRolls
+                .ToDictionary(x => x, x => rolls.Count(y => y == x));
+        }
     }
 }
--- Test/GreedGameTest.cs
+++ Test/GreedGameTest.cs
@@ -1,7 +1,48 @@
-﻿namespace GreedKata.Test
+﻿using Xunit;
+using FluentAssertions;
+using GreedKata.Greed;
+
+namespace GreedKata.Test
 {
     public class GreedGameTest
     {
-        
+        private readonly GreedGame greed = new GreedGame();
+
+        [Fact]
+        public void Calculate_ShouldReturn100_WhenASingle1IsRolled()
+        {
+            int score = greed.Calculate(new int[] {1});
+            score.Should().Be(100);
+        }
+
+        [Fact]
+        public void Calculate_ShouldReturn50_WhenASingle5IsRolled()
+        {
+            int score = greed.Calculate(new int[] {5});
+            score.Should().Be(50);
+        }
+
+        [Theory]
+        [InlineData(new[] {1, 1, 1}, 1000)]
+        [InlineData(new[] {2, 2, 2}, 200)]
+        [InlineData(new[] {3, 3, 3}, 300)]
+        [InlineData(new[] {4, 4, 4}, 400)]
+        [InlineData(new[] {5, 5, 5}, 500)]
+        [InlineData(new[] {6, 6, 6}, 600)]
+        public void Calculate_ShouldReturnTripleScore_WhenThreeOfAKindAreRolled(int[] rolls, int expectedScore)
+        {
+            int score = greed.Calculate(rolls);
+            score.Should().Be(expectedScore);
+        }
+
+        [Theory()]
+        [InlineData(new[] {1, 1, 1, 5, 1}, 1150)]
+        [InlineData(new[] {2, 3, 4, 6, 2}, 0)]
+        [InlineData(new[] {3, 4, 5, 3, 3}, 350)]
+        public void Calculate_ShouldReturnTripleWithSingleScores(int[] rolls, int expectedScore)
+        {
+            int score = greed.Calculate(rolls);
+            score.Should().Be(expectedScore);
+        }
     }
 }
